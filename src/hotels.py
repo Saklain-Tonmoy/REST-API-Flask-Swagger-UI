72b2072dba6b6c84916b2c4c8fd626a27ac6037f
Saklain-Tonmoy <tonmoysaklain@gmail.com>
2022-01-17 01:53:32 +0600

    commit

--- src/hotels.py
+++ src/hotels.py
@@ -14,81 +14,38 @@ def get_all():
     user = getUser(user_email)
     print(user)
     if(user != None):
-        # name = request.json['name']
-        # location = request.json['location']
-        # price = request.json['price']
-        # amenities = request.json['amenities']
-        # sortBy = request.json['sortBy']
-
-
-        # name = request.args.get('name')
-        # location = request.args.get('location')
-        # price = request.args.get('price')
-        # amenities = request.args.getlist('amenities')
-        # sortBy = request.args.getlist('sortBy')
-        
-
-        # print(name)
-        # print(location)
-        # print(price)
-        # print(amenities)
-        # print(sortBy)
 
         hoteldata=[]
 
         name = request.args.get('name')
         location = request.args.get('location')
-        rating = request.args.get('rating')
         price = request.args.get('price')
         sortBy = request.args.getlist('sortBy')
         amenities = ",".join(request.args.getlist('amenities'))
 
-        print(name)
-        print(location)
-        print(price)
-        print(amenities)
-        print(sortBy)
-
         connection = mysqlconnect()
         cursor = connection.cursor()
 
-        sql= "SELECT * FROM best_hotels WHERE "
+        sql= "SELECT * FROM best_hotels WHERE id <> 0 "
         
 
         if(name!=None):
-            if(sql=='SELECT * FROM best_hotels WHERE '):
-                sql=sql+"name LIKE"+f"'%{name}%'"
-            else:
-                sql = sql + "AND name LIKE" + f"'%{name}%'"
+            sql += "AND name LIKE" + f"'%{name}%'"
 
         if(location!=None):
-            if (sql == 'SELECT * FROM best_hotels WHERE '):
-                sql=sql+"location LIKE"+f"'%{location}%'"
-            else:
-                sql = sql + "AND location LIKE" + f"'%{location}%'"
-        if(rating!=None):
-            if (sql == 'SELECT * FROM best_hotels WHERE '):
-                sql=sql+"score="+f"'{rating}'"
-            else:
-                sql = sql + "AND rating=" + f"'{rating}'"
+            sql += "AND location LIKE" + f"'%{location}%'"
+            
         if(price!=None):
-            if (sql == 'SELECT * FROM best_hotels WHERE '):
-                sql=sql+"price >="+f"'{price}'"
-            else:
-                sql = sql + "AND price >=" + f"'{price}'"
+            sql += "AND price >=" + f"'{price}'"
+           
         if(amenities!=None):
-            if (sql == 'SELECT * FROM best_hotels WHERE '):
-                sql=sql+"Amenities LIKE"+f"'%{amenities}%'"
-            else:
-                sql = sql + "AND Amenities LIKE" + f"'%{amenities}%'"
-    
+            sql += "AND amenities LIKE" + f"'%{amenities}%'"
 
-        query=sql
-        print(query)
-        cursor.execute(query)
+        print(sql)
+        cursor.execute(sql)
 
         results = cursor.fetchall()
-        # print(results)
+
         for x in results:
             data = {
                 "Hotel name": x[1],
@@ -101,13 +58,12 @@ def get_all():
                 "amenities": x[8]
                 }
             hoteldata.append(data)
-            print(hoteldata)
-        if(len(sortBy) > 1 or sortBy[0] == 'Low to High'):
-            # hoteldata.sort(key=lambda x: x["price"])   
-            hoteldata.sort(key=lambda x: x.get('price'), reverse=True)
-        else:
-            hoteldata.sort(key=lambda x: x.get('price'), reverse=True)
+        
+        if(len(sortBy) != 0):
+            if(len(sortBy) > 1 or sortBy[0] == 'Low to High'):
+                hoteldata.sort(key = lambda x: x["price"])   
+            else:
+                sorted(hoteldata, key = lambda x : x["price"], reverse=True)
         json = jsonify(hoteldata)
         return json
-        return { "message" : "User Verified" }
     return {"hotels": []}
